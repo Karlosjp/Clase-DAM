318e72b235d870f15f7bace8b3d40631336047e9
Karlosjp <carlosjpinfor@gmail.com>
2017-10-25 10:15:29 +0200

    Multimedia

--- Clase-DAM-2/Multimedia y Dispositivos Moviles/Visual Studio/ConsoleApplication1/ConsoleApplication2/Program.cs
+++ Clase-DAM-2/Multimedia y Dispositivos Moviles/Visual Studio/ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -10,7 +10,27 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            String[] nombres = {"Carlos", "Carlota", "David", "Fabiola", "Javier"};
+            string[] nombres = {"Carlos", "Carlota", "David", "Fabiola", "Javier"};
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                for (int j = 0; i < (nombres.Length - 2) - i; j++)
+                {
+                    if (nombres[j].CompareTo(nombres[j + 1]) > 0)
+                    {
+                        string aux = nombres[j];
+                        nombres[j] = nombres[j + 1];
+                        nombres[j + 1] = aux;
+                    }
+                }
+            }
+
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
