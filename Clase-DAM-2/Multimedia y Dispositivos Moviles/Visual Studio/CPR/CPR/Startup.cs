3263565558c93b4c340377b340e7ae996abd1b20
Karlosjp <carlosjpinfor@gmail.com>
2018-04-27 09:52:19 +0200

    No se

---
+++ Clase-DAM-2/Multimedia y Dispositivos Moviles/Visual Studio/CPR/CPR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPR.Startup))]
+namespace CPR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
