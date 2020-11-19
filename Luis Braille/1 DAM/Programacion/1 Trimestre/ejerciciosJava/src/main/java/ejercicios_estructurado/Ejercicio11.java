6a9dcae46cb538280dca80078bae68fb73e6039e
karlosjp <carlosjpinfor@gmail.com>
2020-11-19 10:42:34 +0100

    programacion - ejercicio estructurado 11

--- Luis Braille/1 DAM/Programacion/1 Trimestre/ejerciciosJava/src/main/java/ejercicios_estructurado/Ejercicio11.java
+++ Luis Braille/1 DAM/Programacion/1 Trimestre/ejerciciosJava/src/main/java/ejercicios_estructurado/Ejercicio11.java
@@ -15,9 +15,21 @@ import java.io.IOException;
  */
 public class Ejercicio11 {
     public static void main(String[] args) throws IOException {
-        
-        for (int i = 1; i <= 10; i++) 
-            for (int j = 0; j <= 10; j++) 
-                System.out.println( i +" x " + j + " = " + i*j);
+    	
+        for (int i = 1; i < 10; i+=3) {
+        	for (int j = 1; j < 10; j++) {
+        		for (int x = i; x < i+3; x++)
+        			System.out.print( x +" x " + j + " = " + x*j + "\t");
+        	
+        		System.out.println("");
+        	}
+        	
+        	System.out.println("");
+        }
+        	
     }
 }
+
+
+
+// 1 * 1 = 1   2 * 1 = 2
