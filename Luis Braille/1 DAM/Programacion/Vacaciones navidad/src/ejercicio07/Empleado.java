9a5758cdb629653828194b53b820f38ce278e964
Karlosjp <carlosjpinfor@gmail.com>
2021-01-12 10:36:39 +0100

    programacion ejerciciio 7

---
+++ Luis Braille/1 DAM/Programacion/Vacaciones navidad/src/ejercicio07/Empleado.java
@@ -0,0 +1,9 @@
+package ejercicio07;
+
+public class Empleado {
+	private static double stDouPagoPorHoraExtra;
+	private String strNif, strNombre;
+	private double douSueldoBase, douTipoIRPF;
+	private int intHorasExtras, intNumerosHijos;
+	private char chCasado;
+}
---
+++ Luis Braille/1 DAM/Programacion/Vacaciones navidad/src/ejercicio07/Main.java
@@ -0,0 +1,10 @@
+package ejercicio07;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
