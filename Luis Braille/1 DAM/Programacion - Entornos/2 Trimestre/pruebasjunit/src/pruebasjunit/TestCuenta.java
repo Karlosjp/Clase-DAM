1e36e7d12a4a2d7e59ffd495fa526a1c4cdeaecd
Karlosjp <carlosjpinfor@gmail.com>
2021-01-28 09:58:23 +0100

    inicio ejercicio 14 programacion

---
+++ Luis Braille/1 DAM/Programacion - Entornos/2 Trimestre/pruebasjunit/src/pruebasjunit/TestCuenta.java
@@ -0,0 +1,14 @@
+package pruebasjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCuenta {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
