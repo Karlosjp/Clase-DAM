f10431080ffac152d18ccd7b87723d88b56bc425
Karlosjp <carlosjpinfor@gmail.com>
2021-02-25 19:02:52 +0100

    ejercicios programacion

---
+++ Luis Braille/1 DAM/Programacion - Entornos/2 Trimestre/practicaprogramacion/src/tema5/ejercicio4/Cliente.java
@@ -0,0 +1,17 @@
+package tema5.ejercicio4;
+
+public class Cliente extends Persona {
+	private String srtTelefonoContacto;
+
+	public Cliente(String strNombre, int intEdad, String srtTelefonoContacto) {
+		super(strNombre, intEdad);
+
+		this.srtTelefonoContacto = srtTelefonoContacto;
+	}
+
+	@Override
+	public void mostrar() {
+		super.mostrar();
+		System.out.println("Telefono de contacto: " + srtTelefonoContacto);
+	}
+}
--- Luis Braille/1 DAM/Programacion - Entornos/2 Trimestre/practicaprogramacion/src/tema5/ejercicio4/Main.java
+++ Luis Braille/1 DAM/Programacion - Entornos/2 Trimestre/practicaprogramacion/src/tema5/ejercicio4/Main.java
@@ -1,10 +1,11 @@
 package tema5.ejercicio4;
 
+import java.text.ParseException;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Cliente clie = null;
+º
+        clie.mostrar();
+    }
 }
